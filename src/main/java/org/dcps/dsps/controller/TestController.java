24fb559df578f2d1a7d5b7ba1f843662b5e96400
natuan <tuan.nguyenanh@axonactive.vn>
2016-10-13 14:40:16 +0700

    init map

--- src/main/java/org/dcps/dsps/controller/TestController.java
+++ src/main/java/org/dcps/dsps/controller/TestController.java
@@ -1,14 +1,15 @@
 package org.dcps.dsps.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
 public class TestController {
 
-    @RequestMapping(value = "test", method = RequestMethod.GET)
-    public String changePassword() {
-        return "hello";
+    @RequestMapping(value = "test", method = GET)
+    public String test() {
+        return "index";
     }
 }
