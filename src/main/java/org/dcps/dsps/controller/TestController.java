3184d80e47b00ec2d5ff12f5a2bd07f700125857
Cicada <tuannn0107>
2016-10-23 09:26:30 +0700

    Add common files for View

--- src/main/java/org/dcps/dsps/controller/TestController.java
+++ src/main/java/org/dcps/dsps/controller/TestController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
+@RequestMapping("MillionProject")
 public class TestController {
+    private static final String INDEX_PAGE = "index";
 
     @Value("${map.url}")
     String mapUrl;
@@ -16,6 +18,6 @@ public class TestController {
     @RequestMapping(value = "test", method = GET)
     public String test(ModelMap modelMap) {
         modelMap.put("mapUrl", mapUrl);
-        return "index";
+        return INDEX_PAGE;
     }
 }
