794896ab38be39f1950b20d1a9312e8063a1298e
Cicada <tuannn0107>
2016-10-24 22:51:20 +0700

    update home page

--- src/main/java/org/dcps/dsps/controller/HomeController.java
+++ src/main/java/org/dcps/dsps/controller/HomeController.java
@@ -1,5 +1,7 @@
 package org.dcps.dsps.controller;
 
+import org.dcps.dsps.entity.dao.SuperEvent;
+import org.dcps.dsps.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -26,9 +28,15 @@ public class HomeController {
         return INDEX_PAGE;
     }
 
-    @RequestMapping(value = "home", method = GET)
-    public String settingSuperEvent(ModelMap modelMap) {
+    @RequestMapping(value = "superEvent", method = GET)
+    public String superEvent(ModelMap modelMap) {
+        SuperEvent superEvent = new SuperEvent();
+        superEvent.setId(1l);
+        superEvent.setName("Hoi Nghi APEC");
+        superEvent.setStartTime(DateUtils.convertStringToDate("01/09/2017", "dd/MM/yyyy"));
+        superEvent.setEndTime(DateUtils.convertStringToDate("30/09/2017", "dd/MM/yyyy"));
         modelMap.put("mapUrl", mapUrl);
+        modelMap.put("superEventOutputForm", superEvent);
         return HOME_PAGE;
     }
 }
