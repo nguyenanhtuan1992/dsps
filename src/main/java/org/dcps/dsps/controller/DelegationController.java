1928dfa427147005939eaea0e84823cb18cc78f6
Cicada <tuannn0107>
2016-10-26 23:18:07 +0700

    update

--- src/main/java/org/dcps/dsps/controller/DelegationController.java
+++ src/main/java/org/dcps/dsps/controller/DelegationController.java
@@ -1,9 +1,14 @@
 package org.dcps.dsps.controller;
 
+import org.dcps.dsps.entity.dao.Event;
+import org.dcps.dsps.entity.dao.Organization;
+import org.dcps.dsps.entity.dao.Police;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -21,6 +26,30 @@ public class DelegationController {
 
     @RequestMapping(value = "displayDelegation", method = GET)
     public String displayDelegation(ModelMap modelMap) {
+
+        /* dummy data */
+        List<Event> listEvents = new ArrayList<Event>();
+        Event event = null;
+        List<Police> listPolice = new ArrayList<Police>();
+        Police police = null;
+        Organization organization = new Organization();
+        for(int temp = 0; temp < 3; temp ++){
+            event =  new Event();
+            event.setId(Long.valueOf(temp));
+            event.setName("Event " + temp);
+            /*for (int i = 0; i < 10; i++){
+                organization.setName("PC" + i);
+                police = new Police();
+                police.setName("Police " + temp + i);
+                police.setOrganization(organization);
+            }*/
+           /* event.setPolices(listPolice);*/
+            listEvents.add(event);
+        }
+
+
+        /* set data to view */
+        modelMap.put("listEvents", listEvents);
         modelMap.put("mapUrl", mapUrl);
         return DELEGATION_INFOR;
     }
